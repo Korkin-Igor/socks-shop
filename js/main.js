6f07d3a4d2182d1dc801e4d341cc63cbc76c48f9
Korkin-Igor <korkinigorttit@gmail.com>
2026-03-02 21:23:59 +0700

    add Conditional Rendering in project; add derectives v-for, :key

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ let app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
